9284c6344485656d7dc0b02898cec570a3f36b94
darkcross174 <dark-cross@rambler.ru>
2021-05-16 23:38:06 +0500

    change test

--- src/test/java/ru/netology/delivery/test/CardDeliveryNewFunctionalTest.java
+++ src/test/java/ru/netology/delivery/test/CardDeliveryNewFunctionalTest.java
@@ -1,6 +1,7 @@
 package ru.netology.delivery.test;
 
 import com.codeborne.selenide.Condition;
+import com.codeborne.selenide.Selenide;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.openqa.selenium.Keys;
@@ -25,12 +26,12 @@ public class CardDeliveryNewFunctionalTest {
         $("[data-test-id=date] input").setValue(DataGenerator.Registration.getDayVisit(3));
         $("[data-test-id=name] input").setValue("Андреев Алексей");
         $("[data-test-id=phone] input").setValue("+79525140000");
-        $("[data-test-id=agreement] input").click();
+        $("[data-test-id=agreement]").click();
         $("button.button").click();
         $(withText("Успешно!")).waitUntil(Condition.visible, 1500);
         $("[data-test-id=success-notification].notification_content").shouldHave(Condition.exactTextCaseSensitive("Встреча успешно запаланирована на "+DataGenerator.Registration.getDayVisit(3)));
         $("button.button").click();
-        $(withText("У вас уже запланирована встреча на эту дату. Хотите перепланировать?")).waitUntil(Condition.visible, 1500);
+        $(withText("У вас уже запланирована встреча на эту дату. Перепланировать?")).waitUntil(Condition.visible, 1500);
 
     }
 
