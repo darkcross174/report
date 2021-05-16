2407d5167f1bad7e9a8195df9485052f8435438d
darkcross174 <dark-cross@rambler.ru>
2021-05-16 23:58:23 +0500

    change test

--- src/test/java/ru/netology/delivery/test/CardDeliveryNewFunctionalTest.java
+++ src/test/java/ru/netology/delivery/test/CardDeliveryNewFunctionalTest.java
@@ -28,10 +28,9 @@ public class CardDeliveryNewFunctionalTest {
         $("[data-test-id=phone] input").setValue("+79525140000");
         $("[data-test-id=agreement]").click();
         $("button.button").click();
-        $(withText("Успешно!")).waitUntil(Condition.visible, 1500);
-        $("[data-test-id=success-notification].notification_content").shouldHave(Condition.exactTextCaseSensitive("Встреча успешно запаланирована на "+DataGenerator.Registration.getDayVisit(3)));
+        $(withText("Успешно!")).waitUntil(Condition.visible, 15000);
         $("button.button").click();
-        $(withText("У вас уже запланирована встреча на эту дату. Перепланировать?")).waitUntil(Condition.visible, 1500);
+        $(withText("У вас уже запланирована встреча на другую дату. Перепланировать?")).waitUntil(Condition.visible, 15000);
 
     }
 
