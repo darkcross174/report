027bc609e5c110cca67ddd7bee6462be4d158447
darkcross174 <dark-cross@rambler.ru>
2021-07-07 22:28:56 +0500

    add replan date

--- src/test/java/ru/netology/delivery/test/DeliveryTest.java
+++ src/test/java/ru/netology/delivery/test/DeliveryTest.java
@@ -9,6 +9,7 @@ import org.junit.jupiter.api.*;
 import org.openqa.selenium.Keys;
 import ru.netology.delivery.data.DataGenerator;
 
+import java.time.Duration;
 import java.time.LocalDate;
 import java.time.format.DateTimeFormatter;
 
@@ -46,6 +47,28 @@ class DeliveryTest {
         val secondMeetingDate = DataGenerator.generateDate(daysToAddForSecondMeeting);
         $("[data-test-id=city] input").setValue(DataGenerator.generateCity("ru"));
         $("[data-test-id=date] input").sendKeys(Keys.CONTROL+"A"+Keys.DELETE);
+
+        $("[data-test-id=date] input").setValue(firstMeetingDate);
+        $("[name='name']").setValue(validUser.getName());
+        $("[name='phone']").setValue(validUser.getPhone());
+        $(".checkbox__box").click();
+        $(".button__text").click();
+        if ($(withText("Доставка в выбранный город недоступна")).isDisplayed()) {
+            int i = validUser.getCity().length() - 2;
+            while (i > 0) {
+                $("[data-test-id=city] .input__control").sendKeys(Keys.BACK_SPACE);
+                i--;
+            }
+            $(".menu-item__control").click();
+            $(".button__text").click();
+        }
+        $("[data-test-id=success-notification]>.notification__content").shouldHave(Condition.exactText("Встреча успешно запланирована на " + firstMeetingDate));
+        $("[data-test-id=date] .input__control").sendKeys(Keys.CONTROL+"A"+Keys.DELETE);
+        $("[data-test-id=date] .input__control").setValue(secondMeetingDate);
+        $(".button__text").click();
+        $(withText("Необходимо подтверждение")).shouldBe(Condition.visible, Duration.ofSeconds(15));
+        $("[data-test-id=replan-notification] .button").click();
+        $("[data-test-id=success-notification]>.notification__content").shouldHave(Condition.exactText("Встреча успешно запланирована на " + secondMeetingDate));
         $("[data-test-id=date] input").setValue(DataGenerator.generateDate(3));
         $("[data-test-id=name] input").setValue(DataGenerator.generateName("ru"));
         $("[data-test-id=phone] input").setValue(DataGenerator.generatePhone("ru"));
